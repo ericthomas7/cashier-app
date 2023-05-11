f51e4576a66c6cfba0120f57ed5c3089287e60a3
Eric Thomas <ericthomas@fetch.ly>
2023-05-11 17:42:23 -0300

    added version and namespacing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :cashes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resources :cashes
+    
+    end
+  end
 end
