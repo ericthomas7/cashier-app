b6ca40ec9fb32d2b11eaf9ff49960e619b34b605
Eric Thomas <ericthomas@fetch.ly>
2023-05-15 15:37:26 -0300

    adjustments on api resource

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,30 +11,14 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  config.headers = {
-    Authorization: `Bearer ${token.replace('"',"")}`,
-    // 'Content-Type': 'application/json',
-    Accept: 'application/json',
-  };
+  // const token = localStorage.getItem('token');
+  // config.headers = {
+  //   Authorization: `Bearer ${token.replace('"',"")}`,
+  //   // 'Content-Type': 'application/json',
+  //   Accept: 'application/json',
+  // };
   return config;
 });
-
-
-api.interceptors.response.use(
-  response => response,
-  error => {
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      error.response.config.url !== BASE_URL + '/login'
-    ) {
-      localStorage.clear();
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
 
 
 api.interceptors.response.use(
